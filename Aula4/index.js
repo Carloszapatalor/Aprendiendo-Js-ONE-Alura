bae21af84daf838db1d920eea2417fca84d038e2
Carlos <carloszapatalor@gmail.com>
2023-03-14 11:37:44 -0600

    Clases y sus atributos

--- Aula4/index.js
+++ Aula4/index.js
@@ -14,15 +14,31 @@
 class Clientes{
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
+    rutCliente;
+}
+
+class cuentaCorriente{
+    numero;
+    saldo;
+    agencia;
+
+    depositoCuenta(valor){
+       this.saldo += valor
+    }
+
 }
 
 const cliente = new Clientes();
+const cuenta = new cuentaCorriente();
 
 cliente.nombreCliente = "Leonardo";
 cliente.dniCliente = "1240003332";
-cliente.numeroCuenta = "1235132536123";
-cliente.saldoCuenta= 1000;
+cliente.rutCliente="34234234";
+cuenta.numero = "1235132536123";
+cuenta.saldo= 1000;
+cuenta.agencia="hey";
 
 console.log(cliente);
+console.log(cuenta.saldo);
+cuenta.depositoCuenta(100);
+console.log (cuenta.saldo);
